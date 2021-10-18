ace8ca453a718ca1c927de12f007f6efb450c279
Andrea Zorzetto <andrea@thatpersonalmail.com>
2021-10-18 15:54:17 +0100

    install image registry

--- user-script.sh
+++ user-script.sh
@@ -65,3 +65,8 @@ snap install helm --classic
 helm repo add jenkins https://charts.jenkins.io
 until kubectl get nodes; do sleep 1; done #Wait for the cluster to be ready
 helm upgrade --install jenkins jenkins/jenkins --version 3.7.1 -n jenkins --create-namespace --set controller.adminPassword="admin" --set controller.serviceType=NodePort --set controller.nodePort=30080
+
+
+##---------------- Registry -----------------------##
+kubectl run registry --image registry:2 -n default
+kubectl expose pod/registry --port 5000 --name registry -n default
